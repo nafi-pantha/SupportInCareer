9a4e5e18ad6dfc53d007f76318c59520526413cc
JMSajib <jm.sajib012@gmail.com>
2017-11-04 02:02:50 +0600

    userform

--- online_exam/exam/views.py
+++ online_exam/exam/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render,get_object_or_404
 from django.http import HttpResponse
-from django.views.generic import (TemplateView,ListView,
-DetailView,CreateView,UpdateView,DeleteView)
+from exam.allViews import UserInfoView
 
 # Create your views here.
 def index(request):
