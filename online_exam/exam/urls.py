1582895c4cc1923d0a51e865a0abc640cadf03c2
nafi-pantha <nafpantha@gmail.com>
2018-01-19 19:37:27 +0600

    Latest Pull

--- online_exam/exam/urls.py
+++ online_exam/exam/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url
 from exam import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^$',views.userLogin,name='home'),
@@ -65,6 +66,21 @@ urlpatterns = [
     url(r'^user_type/$',views.getUserType,name='user_type'),
     url(r'^total_ques_no/$',views.getTotalQuesNo,name='total_ques_no'),
     url(r'^test_type/$',views.getTestType,name='test_type'),
+
+    #email verification
+    url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        views.activate, name='activate'),
+    #password reset
+    url(r'^password_reset/$', auth_views.password_reset,
+        {'template_name':'exam/password_reset_form.html',
+        'password_reset_form': views.EmailValidationOnForgotPassword},name='password_reset'),
+    url(r'^password_reset/done/$', auth_views.password_reset_done,
+        {'template_name':'exam/password_reset_done.html'} ,name='password_reset_done'),
+    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        auth_views.password_reset_confirm,{'template_name':'exam/password_reset_confirm.html'},
+        name='password_reset_confirm'),
+    url(r'^reset/done/$', auth_views.password_reset_complete,
+        {'template_name':'exam/password_reset_complete.html'} ,name='password_reset_complete'),    
 
 
 ]
--- online_exam/exam/views/RegistrationView.py
+++ online_exam/exam/views/RegistrationView.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render,get_object_or_404
 from django.http import HttpResponse, JsonResponse
 from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.forms import PasswordResetForm
 from django.contrib.auth.models import User
 from exam.models import UserInfo
 from django.contrib import messages
 import datetime
+# email verification
+
+from django.contrib.sites.shortcuts import get_current_site
+from django.utils.encoding import force_bytes, force_text
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.template.loader import render_to_string
+from exam.tokens import account_activation_token
+from django.core.mail import EmailMessage
+from django.core.exceptions import ValidationError
 
 def userRegistration(request):
     registered = False
@@ -31,6 +41,19 @@ def userRegistration(request):
 
                 user_info = UserInfo.objects.create(user_id=user.pk,user_contact=userContact,user_image="Null",
                 approver="Null",datetime=datetime.datetime.now())
+                current_site = get_current_site(request)
+                mail_subject = 'Activate your blog account.'
+                message = render_to_string('acc_active_email.html', {
+                    'user': user,
+                    'domain': current_site.domain,
+                    'uid':urlsafe_base64_encode(force_bytes(user.pk)),
+                    'token':account_activation_token.make_token(user),
+                })
+                to_email = email
+                email = EmailMessage(
+                            mail_subject, message, to=[to_email]
+                )
+                email.send()
                 registered=True
                 #messages.add_message(request, messages.INFO, 'Account Created Successfully')
                 return JsonResponse({'status': '1'})
@@ -40,6 +63,19 @@ def userRegistration(request):
             #return HttpResponse("<h3>Username Already Exist.Please Select another one.</h3>")
             return JsonResponse({'status': '2'})
     return render(request,'exam/login.html',{'registered':registered})
+
+def activate(request, uidb64, token):
+    try:
+        uid = force_text(urlsafe_base64_decode(uidb64))
+        user = User.objects.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
+    if user is not None and account_activation_token.check_token(user, token):
+        user.is_active = True
+        user.save()
+        return HttpResponse('Thank you for your email confirmation. Now you can login your account.')
+    else:
+        return HttpResponse('Activation link is invalid!')
 
 
 def emailAvailableCheck(request):
@@ -64,3 +100,11 @@ def usernameAvailableCheck(request):
             return JsonResponse("true", safe=False)
     else:
         return HttpResponse("Problem")
+
+class EmailValidationOnForgotPassword(PasswordResetForm):
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        if not User.objects.filter(email__iexact=email, is_active=True).exists():
+            raise ValidationError("There is no user registered with the specified email address!")
+
+        return email
